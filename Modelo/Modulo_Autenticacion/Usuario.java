0b77ac2798e71ad662dead64acc395f5f37826f9
Cas260208 <caszero.260208@gmail.com>
2024-02-11 14:44:20 -0600

    Clase Usuario

--- Modelo/Modulo_Autenticacion/Usuario.java
+++ Modelo/Modulo_Autenticacion/Usuario.java
@@ -7,4 +7,64 @@ public class Usuario {
     private String contrasenia;
     private String genero; 
     private FechaNac feNac; 
+
+    public Usuario(String nombre, String apellidos, String correo, String contrasenia, String genero, FechaNac feNac) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.correo = correo;
+        this.contrasenia = contrasenia;
+        this.genero = genero;
+        this.feNac = feNac;
+    }
+
+    public Usuario(){}
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setapellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getCorreo() {
+        return correo; 
+    }
+
+    public void setContrasenia(String contrasenia) {
+        this.contrasenia = contrasenia;
+    }
+
+    public String getContrasenia() {
+        return contrasenia;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setFeNac(FechaNac feNac) {
+        this.feNac = feNac;
+    }
+
+    public FechaNac getFeNac() {
+        return feNac;
+    }
+
 }
