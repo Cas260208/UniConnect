4429a9b7104278f187a4e2145fef223272feb5ef
Cas260208 <caszero.260208@gmail.com>
2024-02-12 11:03:47 -0600

    Codigo gestor notificaciones

--- Modelo/Modulo_Notificaciones/TablaNotificaciones.java
+++ Modelo/Modulo_Notificaciones/TablaNotificaciones.java
@@ -1,5 +1,55 @@
 package Modelo.Modulo_Notificaciones;
 
 public class TablaNotificaciones {
-    
+    // Atributos 
+    private String[] interacciones;
+    private String[] solicitudes;
+    private String[] notificaciones;
+
+    // Constructores 
+    public TablaNotificaciones(String[] interacciones, String[] solicitudes, String[] notificaciones) {
+        this.interacciones = interacciones;
+        this.notificaciones = notificaciones;
+        this.solicitudes = solicitudes;
+    }
+
+    public TablaNotificaciones() {}
+
+    // Setters y getters 
+    public void setInteracciones(String[] interacciones) {
+        this.interacciones = interacciones;
+    }
+
+    public String[] getInteracciones() {
+        return interacciones;
+    }
+
+    public void setSolicitudes(String[] solicitudes) {
+        this.solicitudes = solicitudes;
+    }
+
+    public String[] getSolicitudes() {
+        return solicitudes;
+    }
+
+    public void setNotificaciones(String[] notificaciones) {
+        this.notificaciones = notificaciones;
+    }
+
+    public String[] getNotificaciones() {
+        return notificaciones;
+    }
+
+    //  Metodos 
+    public void buscaInteracciones() {
+
+    }
+
+    public void alamacenaSolicitud() {
+
+    }
+
+    public void buscaNotificacionEspecifica() {
+        
+    }
 }
